2be00495294bfd241cd5145d71aa6ba57b4b91c6
Victor <victorgabrielcastrosilva2015@gmail.com>
2025-09-09 15:29:22 -0300

    Projeto Gerador QrCode

---
+++ projetos_moduloJS_udemy/geradorQRCode/js/script.js
@@ -0,0 +1,41 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img");
+
+function generateQrCode(){
+    const qrCodeInputValue = qrCodeInput.value;
+    if(!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Gerando codigo...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
+
+    // container.classList.add("active");
+    qrCodeImg.addEventListener("load", ()=>{
+        container.classList.add("active");
+        qrCodeBtn.innerText = "Código criado!";
+    })
+}
+
+//gerar pelo botão
+qrCodeBtn.addEventListener("click", ()=>{
+    generateQrCode()
+})
+//gerar a partir do enter
+qrCodeInput.addEventListener("keydown", (e)=>{
+    if(e.code === "Enter"){
+        generateQrCode();
+    }      
+})
+
+
+// Limpar area do QrCode
+qrCodeInput.addEventListener("keyup", ()=>{
+    if(!qrCodeInput.value){
+        container.classList.remove("active");
+        qrCodeBtn.innerText = "Gerar QR Code";
+    }
+})
